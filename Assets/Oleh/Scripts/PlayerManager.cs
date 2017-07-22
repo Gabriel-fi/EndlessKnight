1aa74e9b1a607588fda58e09ef4fb26ca18f9916
Oleh <oleh93@hotmail.com>
2017-07-22 19:14:47 -0400

    Add mountain level, enemy script, spites and music

---
+++ Assets/Oleh/Scripts/PlayerManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();       
+	}
+	
+	// Update is called once per frame
+	void Update () {   
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetBool("State", true);
+            
+        }
+
+        if(Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetBool("State", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetBool("State", true);
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetBool("State", false);
+        }
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Jump", true);
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetBool("Jump", false);
+        }
+    }
+}
---
+++ Assets/Oleh/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+    float horizontal;
+    bool faceRight;
+    // Use this for initialization
+    void Start ()
+    {
+        faceRight = true;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        HandelMovement();
+
+        Flip();
+	}
+
+    void HandelMovement()
+    {
+
+        rb.velocity = new Vector2(horizontal * 3, 0.0f);
+    }
+
+    void Flip()
+    {
+        if(horizontal > 0 && !faceRight || horizontal < 0 && faceRight)
+        {
+            faceRight = !faceRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
